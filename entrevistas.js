857fabb1fa501147570b80fc7e5bb8717a60954e
GaboLindo <129307064+GaboLindo@users.noreply.github.com>
2024-09-02 08:52:33 -0300

    a

---
+++ entrevistas.js
@@ -0,0 +1,40 @@
+let entrevista = {
+    entrevista:"Fulano",
+    entrevistado:"Fulano 2",
+    questoes: [
+       {
+        p1:"Qual faculdade você pretende fazer?",
+        r1:""
+    },
+    {
+        p2:"Qual carro você teria?",
+        r2:""
+    },
+    {
+        p3:"Você pretende continuar morando na mesma cidade no futuro?",
+        r3:""
+    },
+    {
+        p4:"Você moraria fora do país?",
+        r4:""
+    },
+    {
+        p5:"Pretende ter quantos filhos?",
+        r5:""
+    },
+    {
+        p6:"Você abriria um Onlyfans?",
+        r6:""
+    },
+    {
+        p7:"Você faria uma viajem para outro planeta?",
+        r7:""
+    },
+    {
+        p8:"Você preferiria ser o maior youtuber de minecraft ou virar um shake arabe?",
+        r8:""
+    },
+    ] 
+
+
+}
